34ebe772f15cab487c4d455172371c954dfcb01e
Brooks <b.sime@myautobrain.com>
2018-10-17 15:15:32 -0500

    no avatar replaced with initials

--- src/UserList.vue
+++ src/UserList.vue
@@ -3,7 +3,12 @@
     <table class="" style="padding-top: 5px">
       <tbody>
         <tr v-for="user in participants" :key="user.id">
-            <td style="text-align: center;"><img :src="user.imageUrl" class="img-msg"/></td>
+            <td style="text-align: center;">
+              <img v-if="user.imageUrl" :src="user.imageUrl" class="img-msg"/>
+              <div v-else class="no-avatar sc-message--initials">
+                {{ initialsFilter(user.name) }}
+              </div>
+            </td>
             <td>{{user.name}}</td>
         </tr>
       </tbody>
@@ -11,11 +16,17 @@
   </div>
 </template>
 <script>
+import initials from './filters/initials'
 export default {
   props: {
     participants: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    initialsFilter (name) {
+      return initials(name)
     }
   }
 }
@@ -29,7 +40,15 @@ export default {
   }
   .img-msg {
     border-radius: 50%;
-    width: 50px;
-    margin-right: 5px;
+    max-width: 50px;
+    margin-right: 15px;
+    margin: 0 auto;
+  }
+
+  .no-avatar {
+    height: 40px;
+    width: 40px;
+    margin: 0 auto;
+    font-size: 20px;
   }
 </style>
